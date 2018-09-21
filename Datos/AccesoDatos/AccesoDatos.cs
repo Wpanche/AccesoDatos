8eaf564fb47921781a83258b8c0e8fde083928c9
wapancheq <wilmar.panche@gmail.com>
2018-09-20 21:05:38 -0500

    servicio base

---
+++ Datos/AccesoDatos/AccesoDatos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using Dapper;
+namespace Datos
+{
+    public class AccesoDatos : Comun
+    {
+        
+
+        public int _Ejecutar(string idStore, object parameters, int usuario, string audiencia)
+        {
+            try
+            {
+                _Verifica_Store_result Permiso = VerificaSp(idStore, usuario, audiencia);
+                if (Permiso.Autorizado)
+                    return SqlMapper.Execute(Connection, Permiso.SP, parameters, commandType: CommandType.StoredProcedure);
+                else
+                    throw new Exception("Usuario no autorizado para la consulta a este modulo");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public dynamic _Listar(string idStore, object parameters, int usuario, string audiencia)
+        {
+            try
+            {
+                _Verifica_Store_result Permiso = VerificaSp(idStore,usuario,audiencia);
+                if (Permiso.Autorizado)
+                    return SqlMapper.Query(Connection, Permiso.SP, parameters, commandType: CommandType.StoredProcedure);
+                else
+                    throw new Exception("Usuario no autorizado para la consulta a este modulo");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public IEnumerable<T> _Listar<T>(string idStore, object parameters, int usuario, string audiencia)
+        {
+            try
+            {
+                _Verifica_Store_result Permiso = VerificaSp(idStore, usuario, audiencia);
+                if (Permiso.Autorizado)
+                    return SqlMapper.Query<T>(Connection, Permiso.SP, parameters, commandType: CommandType.StoredProcedure);
+                else
+                    throw new Exception("Usuario no autorizado para la consulta a este modulo");
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+        
+        protected _Verifica_Store_result VerificaSp(string idStore, int usuario, string audiencia)
+        {
+            try
+            {
+                return SqlMapper.QueryFirst<_Verifica_Store_result>(
+                    Connection,
+                    "Seguridad._Verifica_Store",
+                    new { id = idStore, usuario = usuario, audiencia = audiencia },
+                    commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+    public class _Verifica_Store_result
+    {
+        public string SP { get; set; }
+        public bool Autorizado { get; set; }
+    }
+}
---
+++ Datos/Comun/Comun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Datos
+{
+    public class Comun
+    {
+        protected IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection("User Id=dev;Password=Agosto2018.;Server=192.168.0.6;Database=Desarrollo;");
+            }
+        }
+
+        
+        //Remainder of file is unchanged
+    }
+}
